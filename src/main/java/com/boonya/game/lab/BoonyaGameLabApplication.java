6523d444a96bac2421e18d2a2d59bbfb5e842cd7
boonya <boonya@sina.com>
2025-09-16 19:28:43 +0800

    first commit.

---
+++ src/main/java/com/boonya/game/lab/BoonyaGameLabApplication.java
@@ -0,0 +1,13 @@
+package com.boonya.game.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoonyaGameLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoonyaGameLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/boonya/game/lab/BoonyaGameLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boonya.game.lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoonyaGameLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
